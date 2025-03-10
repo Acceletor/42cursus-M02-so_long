76af9e9fcb72768e5a2dde7729f21abcd25153ea
Karintorn Suebthawilkul <ksuebtha@c1r13s1.42warsaw.pl>
2025-03-10 12:49:55 +0100

    should work on linux now

--- sources/main.c
+++ sources/main.c
@@ -1,4 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksuebtha <ksuebtha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 12:27:12 by ksuebtha          #+#    #+#             */
+/*   Updated: 2025/03/10 12:46:48 by ksuebtha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../minilibx-linux/mlx.h"
+#include <stdlib.h> // Needed for exit()
+
+int	close_window(void *param)
+{
+	(void)param;
+	exit(0); // Terminates the program when the window is closed
+}
 
 int	main(void)
 {
@@ -6,7 +25,9 @@ int	main(void)
 	void	*mlx_win;
 
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-
-	mlx_loop(mlx_win);
+	if (!mlx)
+		return (1);
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	mlx_hook(mlx_win, 17, 0, close_window, NULL);
+	mlx_loop(mlx);
 }
