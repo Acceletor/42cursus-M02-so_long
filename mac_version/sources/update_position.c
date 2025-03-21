b48842480f63f02c8e3dc906f31190028605e804
Karintorn Suebthawilkul <karinsueb@gmail.com>
2025-03-21 17:45:53 +0100

    split update_pos function

--- mac_version/sources/update_position.c
+++ mac_version/sources/update_position.c
@@ -12,6 +12,27 @@
 
 #include "../include/main.h"
 
+static void move_player(t_vars *vars, t_player *p1 , int new_i, int new_j)
+{
+	p1->x = new_j * vars->base->w;
+	p1->y = new_i * vars->base->h;
+	if (vars->map->grid[new_i][new_j] == 'C')
+	{
+		vars->map->grid[new_i][new_j] = '0';
+		ft_printf("\033[33m[You have collected coin]\033[0m\n");
+	}
+	vars->p1->move = 1;
+	vars->p1->move_count++;
+	ft_printf("Current Move: %d\n", vars->p1->move_count);
+	if (vars->map->grid[new_i][new_j] == 'E' && vars->end == true)
+	{
+		ft_printf("\033[35m[You Won!!]\033[0m\n");
+		vars->endgame = true;
+		vars->should_quit_next_frame = true;
+	}
+}
+
+
 void	update_pos(t_vars *vars, int keycode, t_player *p1)
 {
 	int	new_j;
@@ -34,24 +55,7 @@ void	update_pos(t_vars *vars, int keycode, t_player *p1)
 		&& vars->map->grid[i][j + 1] != '1')
 		new_j++;
 	if (new_i != i || new_j != j)
-	{
-		p1->x = new_j * vars->base->w;
-		p1->y = new_i * vars->base->h;
-		if (vars->map->grid[new_i][new_j] == 'C')
-		{
-			vars->map->grid[new_i][new_j] = '0';
-			ft_printf("\033[33m[You have collected coin]\033[0m\n");
-		}
-		vars->p1->move = 1;
-		vars->p1->move_count++;
-		ft_printf("Current Move: %d\n", vars->p1->move_count);
-		if (vars->map->grid[new_i][new_j] == 'E' && vars->end == true)
-		{
-			ft_printf("\033[35m[You Won!!]\033[0m\n");
-			vars->endgame = true;
-			vars->should_quit_next_frame = true;
-		}
-	}
+		move_player(vars, p1, new_i, new_j);
 }
 
 
